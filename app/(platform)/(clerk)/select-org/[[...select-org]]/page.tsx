2fab7ff6e2d0b364ff5b53ba7ba573c33e28e1c9
Ryan Stewart <ryanstew17@gmail.com>
2024-11-19 15:08:58 -0400

    create org page

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,16 @@
+
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganizationPage() {
+  return (
+    <div>
+<OrganizationList
+hidePersonal
+afterSelectOrganizationUrl="/organization/:id"
+afterCreateOrganizationUrl="/organization/:id"
+/>
+ 
+    </div>
+ 
+  );
+}

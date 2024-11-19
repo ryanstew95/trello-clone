60ead050ad9377430cf69f973959b3d46ec40601
Ryan Stewart <ryanstew17@gmail.com>
2024-11-19 12:18:24 -0400

    added a favcon

---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: 'Taskify',
+  description: "Collaborate, manager projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique — accomplish it all with Taskify.",
+}

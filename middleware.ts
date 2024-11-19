37bfa34cd0ae468d107e0d584f174d4cafaab486
Ryan Stewart <ryanstew17@gmail.com>
2024-11-19 17:28:54 -0400

    getting ready to make sidebar

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,10 @@
-// import { authMiddleware } from "@clerk/nextjs";
-// import { NextResponse } from "next/server";
-
-// export default authMiddleware({
-//   publicRoutes: ["/"],
-//   afterAuth(auth, req) {
-//     if (auth.userId && auth.isPublicRoute) {
-//       let path = "/select-org";
-//       if (auth.orgId) {
-//         path = `/organization/${auth.orgId}`;
-//       }
-
-//       const orgSelection = new URL(path, req.url);
-//       return NextResponse.redirect(orgSelection);
-//     }
-//     if (!auth.userId && !auth.isPublicRoute) {
-//       return redirectToSignIn({ returnBackUrl: req.url });
-//     }
-//     if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
-//       const orgSelection = new URL("/select-org", req.url);
-//       return NextResponse.redirect(orgSelection);
-//     }
-//   }
-// });
-
-// export const config = {
-//   matcher: ['/((?!.+\\.[\\.w]+$|_next).*)'],
-// };
+import { clerkMiddleware } from '@clerk/nextjs/server';
+ 
+export default clerkMiddleware()
+ 
+export const config = {
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
+    '/', // Run middleware on index page
+    '/(api|trpc)(.*)'], // Run middleware on API routes
+};

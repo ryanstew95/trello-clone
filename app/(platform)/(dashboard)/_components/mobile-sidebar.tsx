7204136fd709fa201c54318b5667ec3ab17621a9
Ryan Stewart <ryanstew17@gmail.com>
2024-11-20 09:14:08 -0400

    mobile side bar

---
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Menu, Sidebar } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger, SheetTitle, SheetDescription, SheetHeader } from "@/components/ui/sheet";
+
+export const MobileSidebar = () => {
+  const pathname = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+
+const onOpen = useMobileSidebar((state) => state.onOpen);
+const onClose = useMobileSidebar((state) => state.onClose);
+const isOpen = useMobileSidebar((state) => state.isOpen);
+
+useEffect(() => {
+  setIsMounted(true);
+}, []);
+
+useEffect(() => { onClose(); }, [pathname, onClose]);
+
+if (!isMounted) {
+  return null;
+};
+
+  return (
+    <>
+    <Button
+    onClick={onOpen}
+    className="block md:hidden mr-2"
+    variant="ghost"
+    size="sm"
+    >
+<Menu className="h-4 w-4"/>
+    </Button>
+<Sheet open={isOpen} onOpenChange={onClose}>
+  <SheetTrigger></SheetTrigger>
+  <SheetContent
+  side="left"
+  className="p-2 pt-10"
+  >
+    <SheetHeader>
+      <SheetTitle>Your Title Here</SheetTitle>
+      <SheetDescription>Optional description here</SheetDescription>
+    </SheetHeader>
+    <Sidebar />
+  </SheetContent>
+</Sheet>
+    </>
+  )
+};
--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,103 @@
 "use client";
 
-export const Sidebar = () => {
-return (
-  <div>
-    Sidebar!
-  </div>
-)
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+import { NavItem, Organization } from "./nav-item";
+
+interface SidebarProps {
+  storageKey?: string;
 };
+
+export const Sidebar = (
+  { storageKey = "t-sidebar-state",  }: SidebarProps
+) => {
+
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(storageKey, {});
+  const { organization: activeOrganization,
+    isLoaded: isLoadedOrg
+   } = useOrganization();
+   const {
+    userMemberships,
+    isLoaded: isLoadedOrgList
+   } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    }
+   });
+
+const defaultAccordingValue: string[] = Object.keys(expanded).reduce((acc: string[], key: string) => {
+  if (expanded[key]) {
+    acc.push(key);
+  }
+  return acc;
+}, []);
+
+const onExpand = (id: string) => {
+setExpanded((curr) => ({
+  ...curr,
+  [id]: !expanded[id],
+}));
+};
+
+if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+
+return (
+  <>
+    <Skeleton />
+  </>
+);
+}
+return (
+  <>
+    <div className="font-medium text-xs flex items-center mb-1">
+      <span className="pl-4">Workspaces</span>
+      <Button
+        asChild
+        type="button"
+        size="icon"
+        variant="ghost"
+        className="ml-auto"
+      >
+        <Link href="/select-org">
+          <Plus className="h-4 w-4" />
+        </Link>
+      </Button>
+    </div>
+    <Accordion
+      type="multiple"
+      defaultValue={defaultAccordingValue}
+      className="space-y-1"
+    >
+  {userMemberships.data.map((membership) => {
+  const organization: Organization = {
+    id: membership.organization.id, // Access id from organization object
+    // slug: membership.organization.slug, // Access slug from organization object
+    name: membership.organization.name, // Access name from organization object
+    imageUrl: membership.organization.imageUrl, // Access imageUrl from organization object
+  };
+  // !find out where slug is 
+        console.log("DATA:", userMemberships.data);
+
+        return (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id] || false}
+            organization={organization}
+            onExpand={onExpand}
+          />
+        );
+      })}
+    </Accordion>
+  </>
+);
+    }
+    
